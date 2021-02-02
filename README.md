ed761fca745f51bf81e96af6287b8ced300c9d14
Syed <>
2021-02-02 09:22:12 -0500

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# PortfolioWebsite
- Portfolio [website](4bdul.com) made with react and react-spring library. 
---
+++ precache-manifest.9fbd39a3a7ecd7f7c703b8841e68fdbf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2338b1e4cf87d676c704c804154732",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "7f4d7cd3b9d57bc91460",
+    "url": "/Portfolio/static/css/main.6339303f.chunk.css"
+  },
+  {
+    "revision": "00384098260c69a11e6f",
+    "url": "/Portfolio/static/js/2.988301eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.988301eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f4d7cd3b9d57bc91460",
+    "url": "/Portfolio/static/js/main.3b253c8a.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "9d109fd45f11b25e903b6f10381e0059",
+    "url": "/Portfolio/static/media/Resume.9d109fd4.pdf"
+  },
+  {
+    "revision": "4a2775dd45418a47631ccb4e458b067b",
+    "url": "/Portfolio/static/media/Tavisha.4a2775dd.otf"
+  },
+  {
+    "revision": "e373bc70148141aa222492954eeac90e",
+    "url": "/Portfolio/static/media/bg.e373bc70.jpg"
+  }
+]);
